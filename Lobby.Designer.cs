dcd388f95008f9864feec38b750ad17bdb633da0
Gabo Nomeva <144188866+grycs18@users.noreply.github.com>
2024-05-20 19:50:21 -0700

    Lobby.Designer.cs

--- Lobby.Designer.cs
+++ Lobby.Designer.cs
@@ -1,11 +1,3 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Administrator
- * Date: 5/14/2024
- * Time: 11:05 AM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
 namespace HangMath
 {
 	partial class Lobby
